26af5f30ff2074aaa16c73e14174cf2cd4329df3
brayenid <bfenomenal@gmail.com>
2023-01-27 11:27:19 +0800

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 require('dotenv').config()
 
 const Hapi = require('@hapi/hapi')
+const ClientError = require('./exceptions/ClientError')
+
 const notes = require('./api/notes')
 const NotesService = require('./services/postgres/NotesService')
 const NotesValidator = require('./validator/notes')
 
+const users = require('./api/users')
+const UsersService = require('./services/postgres/UsersService')
+const UsersValidator = require('./validator/users')
+
 const init = async () => {
   const notesService = new NotesService()
+  const usersService = new UsersService()
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,12 +25,48 @@ const init = async () => {
     }
   })
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator
+      }
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator
+      }
     }
+  ])
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request
+
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message
+        })
+        newResponse.code(response.statusCode)
+        return newResponse
+      }
+
+      if (!response.isServer) {
+        return h.continue
+      }
+
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami'
+      })
+      newResponse.code(500)
+      return newResponse
+    }
+    return h.continue
   })
 
   await server.start()
